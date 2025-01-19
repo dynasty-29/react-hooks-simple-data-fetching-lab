e5bcd275796ae15f017d2f1f10836dab2505aba1
SonnieCodes <mgathoni.gathoni9@gmail.com>
2025-01-19 12:30:02 +0300

    fetch with use effect

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App() {
+  const [pic, setPic] = useState(null);
+  const[isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json())
+      .then((data) => {
+        setPic(data.message);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <img src={pic} alt="A Random Dog" />
+  );
+}
+export default App;
